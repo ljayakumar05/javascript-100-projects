f60ad97d2643be4415758e42ab14c7592514a853
Jayakumar Loganathan <ljayakumar05@gmail.com>
2026-09-01 21:29:27 +0530

    test javascript

--- 01_age-checker/script.js
+++ 01_age-checker/script.js
@@ -1,5 +1,36 @@
 console.log('Welocme JAI');
 
-alert(5+2);
+let a, b, c;
 
-console.log("welcome to new page")
+a = 5;
+b = 2;
+c = a + b;
+
+console.log(c);
+
+document.getElementById('demo1').innerHTML ="Welcome Jai!";
+
+console.log(typeof true);
+
+console.log(14 === 14 || 8 <= 6);
+
+const isloggedIn = false; 
+
+console.log(!isloggedIn);
+
+let count = 5;
+count --;
+console.log(count);
+
+const age = 22;
+
+const result = age >= 18 ? "Adult" : "Minor";
+console.log(result);
+
+const firstName = "Jaya";
+const lastName = "kumar";
+console.log(firstName + " " +lastName);
+
+const Name = "jayakumar";
+const jkage = 30;
+console.log(`My name is ${Name} and I am ${jkage} year old.`)
