be68ecdaff11c4dfa1dd5482bc2b05cb98ad41cb
Jayakumar Loganathan <ljayakumar05@gmail.com>
2026-09-02 22:28:01 +0530

    added js functions

--- 01_age-checker/script.js
+++ 01_age-checker/script.js
@@ -34,3 +34,43 @@ console.log(firstName + " " +lastName);
 const Name = "jayakumar";
 const jkage = 30;
 console.log(`My name is ${Name} and I am ${jkage} year old.`)
+
+const day = "Monday";
+
+switch (day) {
+    case "Monday":
+        console.log("Start of week");
+        break;
+    case "Thursday":
+        console.log("Almost Weekend");
+        break;
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let j = 5; j >= 1; j--){
+    console.log(j);
+}
+
+function greet(){
+    console.log("Hello!");
+}
+greet();
+
+function greets(name){
+    console.log(`Hello ${name}`);
+}
+greets("jai");
+
+function add(a, b){
+    console.log(a + b);
+}
+add(5, 6);
+
+function adds(a, b){
+    return a + b;
+}
+const resultval = adds(10, 20);
+console.log(resultval);
