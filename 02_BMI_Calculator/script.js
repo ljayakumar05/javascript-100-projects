a4f40fc8f19ee298f70b8832a596961e726d6ca8
Jayakumar Loganathan <ljayakumar05@gmail.com>
2026-09-08 13:29:45 +0530

    BMI functions add

--- 02_BMI_Calculator/script.js
+++ 02_BMI_Calculator/script.js
@@ -1,1 +1,23 @@
 console.log("BMI Calculator");
+
+function calculateBmi(){
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+    let bmi = (weight / (height * height)) * 703;
+
+    document.getElementById("heading").innerHTML = "Your BMI is :" + bmi;
+    document.getElementById("bmi-output").innerHTML = bmi.toFixed(1);
+
+    if (bmi <= 24.9) {
+        document.getElementById('message').innerHTML = "Yor are underweight";
+    }else if (bmi >= 25 && bmi <= 29.9) {
+        document.getElementById('message').innerHTML = "you are healthy weight"
+    }else {
+        document.getElementById('message').innerHTML = "you are overweight";
+    }
+}
+
+function reload(){
+    window.location.reload();
+}
