250e750ffcdf2b6cae6be5cc89b5423b59bf6f53
Jayakumar Loganathan <ljayakumar05@gmail.com>
2026-09-17 23:46:29 +0530

    add js functions

--- 03_Tip_Calculator/script.js
+++ 03_Tip_Calculator/script.js
@@ -1,1 +1,25 @@
-console.log("BMI Calculator");
+const billAmount = document.getElementById("userAmount");
+const displayValue = document.getElementById("userValueDisplay")
+
+const submitBtn = document.getElementById("submitBtn");
+// const displayValueBtn = document.getElementById("displayBtn");
+
+submitBtn.addEventListener("click",() => {
+    const billAmountValu = billAmount.value;
+    console.log(billAmountValu);
+
+    const displayValueBtninput = displayValue.value;
+    console.log(displayValueBtninput);
+
+    const value1 = Number(billAmountValu);
+    const value2 = Number(displayValueBtninput);
+
+    console.log(value1 + value2);
+})
+
+// displayValueBtn.addEventListener('click', ()=> {
+//     const displayValueBtninput = displayValue.value;
+//     console.log(displayValueBtninput);
+
+//     console.log(displayValueBtninput);
+// })
