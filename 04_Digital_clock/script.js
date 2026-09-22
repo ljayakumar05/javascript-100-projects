05ed3d9730b9b1c330fb7ec8ea3fcd245bf003b7
Jayakumar Loganathan <ljayakumar05@gmail.com>
2026-09-22 23:19:04 +0530

    add UI & functions

--- 04_Digital_clock/script.js
+++ 04_Digital_clock/script.js
@@ -1,1 +1,17 @@
-console.log("Digital clock"); 
+
+//Get Dom elements
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+    
+setInterval(()=>{
+    // Get current date and time
+    let currentTime = new Date();
+
+    // Update Dom
+    hrs.textContent = currentTime.getHours();
+    min.textContent = currentTime.getMinutes();
+    sec.textContent = currentTime.getSeconds();
+
+}, 1000);
